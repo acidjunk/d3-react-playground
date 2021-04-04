4cbe2a08cf6aa8c7e29208b790ffb6192d8bedbc
René Dohmen <acidjunk@gmail.com>
2021-04-04 02:07:08 +0200

    corrected data

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     const [data, setData] = useState([]);
     const generateData = ()=>{
         let arr = [];
-        for(let i =0; i < 30; i++){
+        for(let i =0; i < 10; i++){
             arr.push(Math.round(Math.random()*10));
         };
         setData(arr);
