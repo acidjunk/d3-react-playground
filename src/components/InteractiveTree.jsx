e6d334ab979ffcfbcedaabba027ab7bf408060d1
René Dohmen <acidjunk@gmail.com>
2021-04-09 22:21:29 +0200

    Updated Tree

--- src/components/InteractiveTree.jsx
+++ src/components/InteractiveTree.jsx
@@ -20,6 +20,61 @@ const myTreeData = [
       },
       {
         name: "Level 2: B",
+        children: [
+          {
+            name: "Level 3: A",
+          },
+          {
+            name: "Level 3: B",
+          },
+          {
+            name: "Level 3: C",
+          },
+          {
+            name: "Level 3: D",
+          },
+        ]
+      },
+      {
+        name: "Level 2: C",
+      },
+      {
+        name: "Level 2: D",
+      },
+      {
+        name: "Level 2: E",
+      },
+      {
+        name: "Level 2: F",
+      },
+      {
+        name: "Level 2: G",
+      },
+      {
+        name: "Level 2: H",
+      },
+      {
+        name: "Level 2: I",
+      },
+      {
+        name: "Level 2: J",
+        children: [
+          {
+            name: "Level 3: E",
+          },
+          {
+            name: "Level 3: F",
+          },
+          {
+            name: "Level 3: G",
+          },
+          {
+            name: "Level 3: H",
+          },
+        ]
+      },
+      {
+        name: "Level 2: K",
       },
     ],
   },
